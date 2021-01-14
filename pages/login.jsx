349b975e4725215d9217ee59a113d846160d12f4
hatchwald <fatkhur139wec@gmail.com>
2021-01-14 10:48:10 +0700

    first commit

---
+++ pages/login.jsx
@@ -0,0 +1,115 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import ReactDOM from "react-dom"
+import React, { useState } from 'react';
+import { ToastProvider, useToasts } from 'react-toast-notifications'
+
+export default function Login() {
+
+    const [username, setUsername] = useState({ username: "", password: "" })
+    const [validationPassw, setValidationPassw] = useState("hidden")
+    const [loader, setLoader] = useState({ class: "", attribute: false, loading: "hidden" })
+
+    const checkUser = (e) => {
+        const value = e.target.value;
+        username.username = value;
+        setUsername(username)
+
+    }
+    const passCheck = (e) => {
+        const value = e.target.value;
+        username.password = value;
+        setUsername(username)
+
+    }
+
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+            method: "post",
+
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        })
+
+        return { code: response.status, data: response.json() }
+    }
+
+    const submit_login = (e) => {
+        e.preventDefault()
+        if (username.password == "" || username.username == "") {
+            setValidationPassw("block")
+            return
+        } else {
+            setValidationPassw("hidden")
+            setLoader({ class: "cursor-not-allowed", attribute: true, loading: "block" })
+        }
+
+
+
+        postData("http://localhost:3000/login", { "username": "kyle", "password": "some" }).then(data => {
+            if (data.code != 200) {
+                console.log("error happen")
+                data.data.then(data => {
+                    setLoader({ class: "", attribute: false, loading: "hidden" })
+                })
+            } else {
+                //    
+            }
+
+        }).catch(err => {
+            alert("error get data")
+            console.log(err)
+            setLoader({ class: "", attribute: false, loading: "hidden" })
+        })
+
+    }
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Login Page </title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="w-full max-w-xs">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submit_login}>
+                    <fieldset disabled={loader.attribute}>
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                    </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" onChange={checkUser} type="text" placeholder="Username" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                    </label>
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" autoComplete="true" id="password" type="password" placeholder="******************" onChange={passCheck} />
+                            <p className={`text-red-500 text-xs italic ${validationPassw}`}>Password or username cant be empty.</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button className={`inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${loader.class}`} type="submit">
+                                <svg className={`animate-spin -ml-1 mr-3 h-5 w-5 text-white ${loader.loading}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            Sign In
+                        </button>
+                            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                                Forgot Password?
+                        </a>
+
+                        </div>
+                    </fieldset>
+                </form>
+                <p className="text-center text-gray-500 text-xs">
+                    &copy;2020 Acme Corp. All rights reserved.
+                </p>
+            </div>
+        </div>
+
+    )
+}
---
+++ pages/user.jsx
@@ -0,0 +1,44 @@
+import Head from "next/head"
+import styles from '../styles/Home.module.css'
+
+export default function User() {
+    return (
+        <div className={styles.container} >
+            <Head>
+                <title>User Page </title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="lg:container lg:mx-auto">
+                <div className="inline-block px-4 py-2 m-2 max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                    </div>
+                </div>
+
+                <div className="inline-block px-4 py-2 m-2 max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
