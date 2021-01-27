5ed42682402e0b7734366debfc00d5a50ac2eac3
hatchwald <fatkhur139wec@gmail.com>
2021-01-27 17:37:54 +0700

    > update register page > add validation

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,6 +1,32 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
+import React, { useState } from "react"
 export default function register() {
+    const [alldata, setData] = useState({ username: "", password: "", secpassword: "" })
+    const submitRegister = e => {
+        e.preventDefault()
+        if (alldata.password !== alldata.secpassword) {
+            alert("password not match")
+        }
+    }
+
+    const changeUser = e => {
+        const value = e.target.value
+        alldata.username = value
+        setData(alldata)
+    }
+
+    const changePass = e => {
+        const value = e.target.value
+        alldata.password = value
+        setData(alldata)
+    }
+
+    const change2Pass = e => {
+        const value = e.target.value
+        alldata.secpassword = value
+        setData(alldata)
+    }
     return (
         <div className={styles.container}>
             <Head>
@@ -10,33 +36,36 @@ export default function register() {
             <div className="w-full max-w-xs">
 
 
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit="">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submitRegister}>
                     <fieldset >
-
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                                 Username
-                    </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" onChange="" type="text" placeholder="Username" />
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" onChange={changeUser} type="text" placeholder="Username" />
                         </div>
                         <div className="mb-6">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                 Password
-                    </label>
-                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" autoComplete="true" id="password" type="password" placeholder="******************" onChange="" />
-                            <p className={`text-red-500 text-xs italic `}>Password or username cant be empty.</p>
+                            </label>
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" autoComplete="false" id="password" type="password" placeholder="******************" onChange={changePass} />
+                            <p className={`text-red-500 text-xs italic hidden`}>Password or username cant be empty.</p>
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="">Re-enter Password</label>
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" autoComplete="false" type="password" onChange={change2Pass} />
                         </div>
                         <div className="flex items-center justify-between">
                             <button className={`inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline `} type="submit">
-                                <svg className={`animate-spin -ml-1 mr-3 h-5 w-5 text-white `} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <svg className={`animate-spin -ml-1 mr-3 h-5 w-5 text-white hidden`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
-                            Register
-                        </button>
+                                Register
+                            </button>
                             <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/login">
                                 Login
-                        </a>
+                            </a>
 
                         </div>
                     </fieldset>
